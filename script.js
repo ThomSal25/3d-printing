eb01da3ba994f6285736f7c5eb97061b6c0b63ae
Thomas Salmann <thomas.salmann@gmail.com>
2023-01-06 11:42:30 +0100

    ref: changes kind of slideshow

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const auswahl = document.querySelector("#auswahl");
-const manga = document.querySelectorAll(".manga");
-
-const galery = document.querySelectorAll(".galery2 section");
+const galery = document.querySelectorAll(".galery3 section");
+const root = document.querySelector(":root");
+const duration1 = document.querySelector(".duration");
 
 auswahl.addEventListener("change", hideShow);
 
 function hideShow() {
-    if (auswahl.value === "option_all") {
-        for (i = 0; i < galery.length; i++) {
-            galery[i].classList.remove("hide");
-        }
-     }
-    select("option_manga", "manga");
-    select("option_dragonball", "dragonball");
-    select("option_one_piece", "one_piece")
-    select("option_dc", "dc")
-    select("option_harry_potter", "harry_potter")
-    select("option_stones", "stones")
+  if (auswahl.value === "option_all") {
+    for (i = 0; i < galery.length; i++) {
+      galery[i].classList.remove("hide");
+    }
+  }
+  select("option_manga", "manga", "9s");
+  select("option_dragonball", "dragonball");
+  select("option_one_piece", "one_piece");
+  select("option_dc", "dc");
+  select("option_harry_potter", "harry_potter");
+  select("option_stones", "stones");
 }
 
-function select(option, className) {
+function select(option, className, duration, time) {
   if (auswahl.value === option) {
     for (i = 0; i < galery.length; i++) {
-      hide(className);
+        hide(className);
+        setAnimationDuration(className)
     }
+    root.style.setProperty("--time", time);
+    root.style.setProperty("--duration", duration);
   }
 }
 
@@ -31,6 +34,12 @@ function hide(name) {
   galery[i].classList.remove("hide");
   if (!galery[i].classList.contains(name)) {
     galery[i].classList.add("hide");
-    console.log(galery[i]);
   }
+}
+
+function setAnimationDuration(name) {
+    galery[i].classList.remove("duration");
+    if (galery[i].classList.contains(name)) {
+        galery[i].classList.add("duration"); 
+    }
 }
